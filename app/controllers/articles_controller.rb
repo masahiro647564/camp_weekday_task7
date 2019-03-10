d5cdbce739bb4847fbead3140877fe54d3c6e2d8
masahiro-g <kwoaudis32@gmail.com>
2019-03-10 22:56:30 +0900

    editページ作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,16 @@ class ArticlesController < ApplicationController
     redirect_to @article
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to @article
+  end
+
   private
 
   def article_params
