51d181adc44fd8d75cb4c4a609a17df201c7e795
masahiro-g <kwoaudis32@gmail.com>
2019-03-11 00:02:42 +0900

    機能修正,before_action追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,11 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only [ :show, :edit, :update, :destroy]
-
-  def show
-  end
+  before_action :set_article, only: [ :show, :edit, :update, :destroy]
 
   def index
     @articles = Article.all
+  end
+
+  def show
   end
 
   def new
@@ -36,6 +36,10 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 
   
