b1e1095f06ac82f099dcd54a0c55d53bf6920ace
masahiro-g <kwoaudis32@gmail.com>
2019-03-11 11:59:24 +0900

    機能修正,コメント追加_

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+  #リファクタリングしました｡
   before_action :set_article, only: [ :show, :edit, :update, :destroy]
-
+  
   def index
     @articles = Article.all
   end
